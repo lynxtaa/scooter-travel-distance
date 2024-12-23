78f86c11ae5ac8c391c431890c83bbe8af790193
Alex Kondratiuk <lynxtaa@gmail.com>
2024-12-23 22:17:07 +0100

    chore: add next eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,50 @@
 import eslintConfig from '@lynxtaa/eslint-config'
 import requiresTypechecking from '@lynxtaa/eslint-config/requires-typechecking'
+import nextPlugin from '@next/eslint-plugin-next'
+import reactPlugin from 'eslint-plugin-react'
+import hooksPlugin from 'eslint-plugin-react-hooks'
 
 export default [
-	// TODO: support eslint-config-next
 	...eslintConfig,
 	...requiresTypechecking,
-	// See https://typescript-eslint.io/getting-started/typed-linting
 	{
 		languageOptions: {
 			parserOptions: {
 				projectService: {
-					allowDefaultProject: ['*.js', '*.mjs'],
+					allowDefaultProject: ['*.js', '*.mjs', 'vite.config.mts'],
 				},
 				tsconfigRootDir: import.meta.dirname,
 			},
 		},
 	},
 	{
+		...reactPlugin.configs.flat.recommended,
 		rules: {
-			'@typescript-eslint/unbound-method': 'off',
+			...reactPlugin.configs.flat.recommended.rules,
+			'react/react-in-jsx-scope': 'off',
 		},
+		settings: {
+			react: {
+				version: 'detect',
+			},
+		},
+	},
+	{
+		plugins: {
+			'react-hooks': hooksPlugin,
+		},
+		rules: hooksPlugin.configs.recommended.rules,
+	},
+	{
+		plugins: {
+			'@next/next': nextPlugin,
+		},
+		rules: {
+			...nextPlugin.configs.recommended.rules,
+			...nextPlugin.configs['core-web-vitals'].rules,
+		},
+	},
+	{
+		ignores: ['.next', 'coverage'],
 	},
 ]
